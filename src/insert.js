2a0f62c9c9f1a541ed29e0a6e52642362d0e199c
wangqun6 <wangqun28>
2017-05-31 02:19:32 +0800

    修改细节

--- src/insert.js
+++ src/insert.js
@@ -22,7 +22,7 @@ let jobs = [];
 MongoClient.connect('mongodb://localhost:27017/analysis', function (err, db) {
     if (err) {
         console.log('数据库连接失败');
-        writer.write('==========================\n', err);
+        writer.write(err.toString());
     }
     else {
         console.log('数据库连接成功');
@@ -30,15 +30,15 @@ MongoClient.connect('mongodb://localhost:27017/analysis', function (err, db) {
         fs.readdir('e:/LOGS/', function (err, files) {
             if (err) {
                 console.log(err);
-                writer.write('==========================\n', err);
+                writer.write(err.toString());
             } else {
                 files.forEach(function (filename, i) {
-                    !i && jobs.push(function (done) {
+                    jobs.push(function (done) {
                         console.log('开始读取文件');
                         fs.readFile(`e:/LOGS/${filename}`, 'utf8', (err, content) => {
                             if (err) {
                                 console.log(`在读取${filename}的出错`, err);
-                                writer.write('==========================\n', err);
+                                writer.write(err.toString());
                             } else {
                                 console.log(`读取${filename}成功`);
                                 let lines = content.split('\n');
@@ -50,9 +50,9 @@ MongoClient.connect('mongodb://localhost:27017/analysis', function (err, db) {
                                         let re = str.replace(/"/g, '\\"').replace(/\s/g, "");
                                         line = line.replace(str, re);
 
-                                        let obj = JSON.parse(line);
                                         // 把符合要求的每行数据插入
-                                        if (obj.requestUrl.indexOf('/baseTime.jpg') !== -1) {
+                                        if (obj.indexOf('/baseTime.jpg') !== -1) {
+                                            let obj = JSON.parse(line);
                                             let urlObj = url.parse(obj.requestUrl, true).query;
                                             if (urlObj.page.indexOf('sz.jd.com') !== -1) {
                                                 shouldInsertLines++;
@@ -63,7 +63,7 @@ MongoClient.connect('mongodb://localhost:27017/analysis', function (err, db) {
                                                 obj.st = Number(obj.st);
                                                 obj.bt = Number(obj.bt);
                                                 obj.time = Number(obj.time);
-                                                delete obj.header;
+                                                delete obj.headers;
                                                 let area = ip.findSync(obj.ip);
                                                 obj.country = area[0];
                                                 obj.province = area[1];
@@ -80,6 +80,11 @@ MongoClient.connect('mongodb://localhost:27017/analysis', function (err, db) {
                                                     obj.PC = true;
                                                     pcLines++;
                                                 }
+                                                for (let key in obj) {
+                                                    if (key.indexOf('.') !== -1) {
+                                                        delete obj[key];
+                                                    }
+                                                }
                                                 insertData.push(obj)
                                             }
                                         }
@@ -88,16 +93,18 @@ MongoClient.connect('mongodb://localhost:27017/analysis', function (err, db) {
                                 console.log(`文件${filename}插入数据库`);
                                 col.insertMany(insertData, function (err, r) {
                                     if (err) {
-                                        console.log(`${filename}文件数据在插入数据库过程中出错`);
-                                        writer.write('==========================\n', err);
-                                    } else {
+                                        console.log(`${filename}文件数据在插入数据库过程中出错`, err);
+                                        writer.write(err.toString());
+                                        done();
+                                    }
+                                    else {
                                         szTimeLines += r.insertedCount;
                                         if (r.insertedCount !== insertData.length) {
                                             console.log(`err：文件${filename}应该插入${insertData.length},实际插入${r.insertedCount}`);
-                                            writer.write('==========================\n', `err：文件${filename}应该插入${insertData.length},实际插入${r.insertedCount}`);
+                                            writer.write(`err：文件${filename}应该插入${insertData.length},实际插入${r.insertedCount}`);
                                         } else {
                                             console.log(`文件${filename}中的数据全部插入完毕`);
-                                            writer.write('==========================\n', `文件${filename}中的数据全部插入完毕`);
+                                            writer.write(`文件${filename}中的数据全部插入完毕`);
                                         }
                                         done();
                                     }

cf210d1b3a59e474746a193de8554627886ec0a8
wangqun6 <wangqun28>
2017-05-31 00:19:27 +0800

    代码：将原始数据插入到数据库中

--- src/insert.js
+++ src/insert.js
@@ -8,62 +8,96 @@ let url = require('url');
 let ip = require('../lib/ip');
 ip.load("../data/ip.dat");
 let Obj = require('../lib/obj');
+let task = require('../lib/task');
 
 
 let sumLines = 0;
 let szLines = 0;
+let pcLines = 0;
+
+let writer = fs.createWriteStream('../log/task.log', {flags: 'a'});
+let jobs = [];
 
 MongoClient.connect('mongodb://localhost:27017/analysis', function (err, db) {
     if (err) {
-        console.log(err);
-    } else {
+        console.log('数据库连接失败');
+        writer.write('==========================\n', err);
+    }
+    else {
+        console.log('数据库连接成功');
         let col = db.collection('visit');
         fs.readdir('e:/LOGS/', function (err, files) {
-            // console.log( data );
-            files.forEach(function (filename, i) {
-                // console.log( filename );
-                fs.readFile(`e:/LOGS/${filename}`, 'utf8', (err, content) => {
-                    if (err) {
-                        console.log(`在读取${filename}的出错`, err);
-                    } else {
-                        let lines = content.split('\n');
-                        sumLines += (lines.length - 1);
-                        let insertData = [];
-                        lines.forEach(function (line, i) {
-                            if (line) {
-                                let obj = JSON.parse(line[0]);
-                                // 把符合要求的每行数据插入
-                                if (url.parse(obj.page).hostname === 'sz.jd.com') {
-                                    szLines++;
-                                    Obj.extend(obj, url.parse(obj.requestUrl, true).query);
-                                    obj.wt = Number(obj.wt);
-                                    obj.jt = Number(obj.jt);
-                                    obj.st = Number(obj.st);
-                                    obj.bt = Number(obj.bt);
-                                    obj.time = Number(obj.time);
-                                    delete obj.header;
-                                    let area = ip.findSync(obj.ip);
-                                    obj.country = area[0];
-                                    obj.province = area[1];
-                                    insertData.push(obj)
-                                }
-                            }
-                        });
-                        col.insertMany(insertData, function (err, r) {
+            if (err) {
+                writer.write('==========================\n', err);
+            } else {
+                files.forEach(function (filename) {
+                    jobs.push(function (done) {
+                        fs.readFile(`e:/LOGS/${filename}`, 'utf8', (err, content) => {
                             if (err) {
-                                console.log(`${filename}文件数据在插入过程中出错`);
+                                console.log(`在读取${filename}的出错`, err);
+                                writer.write('==========================\n', err);
                             } else {
-                                if (r.insertedCount !== insertData.length) {
-                                    console.log(`err：文件${filename}应该插入${insertData.length},实际插入${r.insertedCount}`);
-                                } else {
-                                    console.log(`文件${filename}中的数据全部插入完毕`)
-                                }
+                                let lines = content.split('\n');
+                                sumLines += (lines.length - 1);
+                                let insertData = [];
+                                lines.forEach(function (line, i) {
+                                    if (line) {
+                                        let obj = JSON.parse(line[0]);
+                                        // 把符合要求的每行数据插入
+                                        if (obj.requestUrl.indexOf('/baseTime.jpg') !== -1 && obj.requestUrl.search(/https?:\/\/sz\.jd\.com/) !== -1) {
+                                            szLines++;
+                                            obj.wtime = obj.time;
+                                            Obj.extend(obj, url.parse(obj.requestUrl, true).query);
+                                            obj.wt = Number(obj.wt);
+                                            obj.jt = Number(obj.jt);
+                                            obj.st = Number(obj.st);
+                                            obj.bt = Number(obj.bt);
+                                            obj.time = Number(obj.time);
+                                            delete obj.header;
+                                            let area = ip.findSync(obj.ip);
+                                            obj.country = area[0];
+                                            obj.province = area[1];
+                                            if (-1 !== obj.userAgent.indexOf('iPhone')) {
+                                                obj.iPhone = true;
+                                                obj.mobile = true;
+                                            }
+                                            else if (obj.userAgent.indexOf('Android') !== -1) {
+                                                obj.mobile = true;
+                                                obj.Android = true;
+                                            } else {
+                                                obj.mobile = false;
+                                                obj.PC = true;
+                                                pcLines++;
+                                            }
+                                            insertData.push(obj)
+                                        }
+                                    }
+                                });
+                                col.insertMany(insertData, function (err, r) {
+                                    if (err) {
+                                        console.log(`${filename}文件数据在插入数据库过程中出错`);
+                                        writer.write('==========================\n', err);
+                                    } else {
+                                        if (r.insertedCount !== insertData.length) {
+                                            console.log(`err：文件${filename}应该插入${insertData.length},实际插入${r.insertedCount}`);
+                                            writer.write('==========================\n', `err：文件${filename}应该插入${insertData.length},实际插入${r.insertedCount}`);
+                                        } else {
+                                            console.log(`文件${filename}中的数据全部插入完毕`);
+                                            writer.write('==========================\n', `文件${filename}中的数据全部插入完毕`);
+                                        }
+                                        done();
+                                    }
+                                });
                             }
-
                         });
-                    }
+                    });
                 });
-            });
+            }
+        });
+
+        task.q(jobs, function () {
+            db.close();
+            writer.end();
         });
     }
 
